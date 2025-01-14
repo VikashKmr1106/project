3915884e7962b3cb6d86e6dad673c326db5a5d77
Vikash <vikash.kmr1106@gmail.com>
2025-01-14 21:28:41 +0530

    checkout

--- src/pages/TanstackQuery/TranstackQuery.jsx
+++ src/pages/TanstackQuery/TranstackQuery.jsx
@@ -14,6 +14,7 @@ const TranstackQuery = () => {
         throw new Error(error);
       }
     },
+    gcTime: 1000,
 		// staleTime: 5 * 60 * 1000,
     staleTime: Infinity,  // Data never becomes stale
     cacheTime: Infinity,  // Cache data forever unless invalidated
